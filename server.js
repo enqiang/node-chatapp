339b609b1dd2e2fac0547000ad49f1bede3c4b0a
enqiang <qin89754083@gmail.com>
2013-11-02 18:16:33 -0400

    Start up

---
+++ server.js
@@ -0,0 +1,30 @@
+
+//db connect action.
+var db_config = {
+  	host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node_chat'
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+
+  connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    }
+  });
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect(); 
+    } else {
+      throw err;                                 
+    }
+  });
+}
+handleDisconnect();
